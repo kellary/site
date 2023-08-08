96dcd8e924cf2aca7110ddb7b797a8f75bc945ca
kellary <51315509+kellary@users.noreply.github.com>
2023-08-08 17:29:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+document.getElementById('discord').addEventListener('click', function() {
+    window.location.href = ' https://discordapp.com/users/385730747681472514/';
+  });
+  
+  document.getElementById('steam').addEventListener('click', function() {
+    window.location.href = 'https://www.instagram.com/kellary13/';
+  });
+  
+  document.getElementById('twitter').addEventListener('click', function() {
+    window.location.href = 'https://github.com/kellary';
+  });
+  
+  document.getElementById('twitch').addEventListener('click', function() {
+    window.location.href = 'https://twitter.com/kellary13';
+  });
+  
+  document.getElementById('telegram').addEventListener('click', function() {
+    window.location.href = 'https://t.me/flipkips';
+});
+document.getElementById('one').addEventListener('click', function() {
+    window.location.href = ' https://kellary.vercel.app';
+  });
+  
+  document.getElementById('two').addEventListener('click', function() {
+    window.location.href = 'https://practical.vercel.app';
+  });
+  
+  document.getElementById('three').addEventListener('click', function() {
+    window.location.href = 'https://flipkips.vercel.app';
+});
+  
+document.getElementById('four').addEventListener('click', function() {
+  window.location.href = 'https://github.com/kellary/Lava-Monitoring-Server/blob/main/main.js';
+});
